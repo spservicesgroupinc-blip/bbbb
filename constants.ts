845b32d8cdaa0003621b75a0b6992e11c8f39e29
spservicesgroupinc-blip <spservicesgroupinc@gmail.com>
2026-02-09 14:26:16 -0500

    first commit

---
+++ constants.ts
@@ -0,0 +1,3 @@
+
+// Web App URL from Google Apps Script Deployment
+export const GOOGLE_SCRIPT_URL: string ='https://script.google.com/macros/s/AKfycbz6Tbch92_uW7t2w1xk11LfIX66ojLO-qcBXnxfZnA6lsVb9b1FjSLk9YyqU5nlhk8GOg/exec'
